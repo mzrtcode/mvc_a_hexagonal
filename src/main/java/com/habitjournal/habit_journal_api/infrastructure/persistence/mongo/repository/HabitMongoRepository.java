f3f34175109e0abde6f85ca9994bfdad9f20fa62
Stiven M <stvnm33@gmail.com>
2026-05-03 14:21:04 -0500

    wip: migrar la BD de JPA a MongoDB

---
+++ src/main/java/com/habitjournal/habit_journal_api/infrastructure/persistence/mongo/repository/HabitMongoRepository.java
@@ -0,0 +1,10 @@
+package com.habitjournal.habit_journal_api.infrastructure.persistence.mongo.repository;
+
+import com.habitjournal.habit_journal_api.infrastructure.persistence.mongo.document.HabitDocument;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface HabitMongoRepository extends MongoRepository<HabitDocument, Long> {
+    Optional<HabitDocument> findByName(String name);
+}
